a7f38083690397e62e5cf809757fd78512e8a22d
Eroc123 <79434990+Eroc123@users.noreply.github.com>
2021-07-10 17:21:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,101 @@
 from random import randint
-from os import system
 from time import sleep
-
+from cogs import Ai
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel
+from PyQt5.QtCore import pyqtSlot
+import sys
 # 0 rock
 # 1 paper
 # 2 scissors
 w = ['rock','paper', 'scissor']
-system('clear')
 aiwin = 0
 playerwin = 0
 p = {
 0 : 'rock',
 1 : 'paper',
-2 : 'scissor'
+-1 : 'scissor'
 }
 log = {
 'in' : [0],
 'result' : [1],
 'ch' : [1]
 }
-win = [[1,0],[0,2],[2,1]]
-lose = [[0,1],[2,0],[1,2]]
-tie = [[1,1],[2,2],[0,0]]
-def main(rt):
+win = [[1,0],[0,-1],[-1,1]]
+lose = [[0,1],[-1,0],[1,-1]]
+tie = [[1,1],[-1,-1],[0,0]]
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.title = 'RPS AI bot - https://github.com/Eroc123'
+        self.initVar()
+        self.initUI()
+    def initVar(self):
+        self.c = Ai()
+        self.aiwin = 0
+        self.playerwin = 0
+        self.w = ['rock','paper', 'scissor']
+        self.aiwin = 0
+        self.playerwin = 0
+        self.p = {
+        0 : 'rock',
+        1 : 'paper',
+        -1 : 'scissor'
+        }
+        self.log = {
+        'in' : [0],
+        'result' : [1],
+        'ch' : [1]
+        }
+        self.win = [[1,0],[0,-1],[-1,1]]
+        self.lose = [[0,1],[-1,0],[1,-1]]
+        self.tie = [[1,1],[-1,-1],[0,0]]
+        self.scores = 'YOU : {} AI : {}'.format(self.playerwin, self.aiwin)
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        button1 = QPushButton('Rock', self)
+        button1.setToolTip('Throw Rock against the AI')
+        button1.clicked.connect(self.rock)
+        button1.move(50,100)
+        button2 = QPushButton('Paper', self)
+        button2.setToolTip('Throw Paper against the AI')
+        button2.clicked.connect(self.paper)
+        button2.move(150,100)
+        button3 = QPushButton('Scissor', self)
+        button3.setToolTip('Throw Scissor against the AI')
+        button3.clicked.connect(self.rock)
+        button3.move(250,100)
+        self.scoreLabel = QLabel(self.scores, self)
+        self.scoreLabel.move(150, 50)
+        self.show()
+    @pyqtSlot()
+    def rock(self):
+        self.pchoice = 0
+        self.get_result()
+    @pyqtSlot()
+    def paper(self):
+        self.pchoice = 1
+        self.get_result()
+    @pyqtSlot()
+    def scissor(self):
+        self.pchoice = -1
+        self.get_result()
+    def get_result(self):
+        self.aichoice = self.c.predict()
+        self.result = check([self.pchoice, self.aichoice])
+        self.c.log(self.aichoice, self.pchoice, self.result)
+        if self.result == -1:
+            self.aiwin += 1
+        if self.result == 1:
+            self.playerwin += 1
+        self.update_result()
+    def update_result(self):
+        self.scores = 'YOU : {} AI : {}'.format(self.playerwin, self.aiwin)
+        self.scoreLabel.setText(self.scores)
+def check(rt):
     if rt in win:
         return 1
     elif rt in lose:
@@ -35,7 +108,7 @@ def rps(input):
     elif input == 'paper':
         return 1
     elif input == 'scissor':
-        return 2
+        return -1
 def getinput():
     inpt = input('rock, paper or scissor\n')
     if inpt in w:
@@ -43,155 +116,8 @@ def getinput():
     else:
         print('Unknown input, exiting..')
         exit()
-d
-while True:
-    choice = rps(getinput())
 
-    aichoicec = randint(0,5)
-    if len(log['in']) > 5:
-        if log['in'][-1] == log['in'][-2]:
-            if log['in'][-1] == log['in'][-4] and log['in'][-2] == log['in'][-5]:
-                if log['in'][-3] == 0:
-                    aichoice = 1
-                elif log['in'][-3] == 1:
-                    aichoice = 2
-                elif log['in'][-3] == 2:
-                    aichoice = 0
-            elif log['in'][-2] == log['in'][-3] == log['in'][-4]:
-                if log['in'][-1] == 0:
-                    aichoice = 1
-                elif log['in'][-1] == 1:
-                    aichoice = 2
-                elif log['in'][-1] == 2:
-                    aichoice = 0
-            elif log['in'][-3] == log['in'][-4]:
-                if log['in'][-4] == 0:
-                    aichoice = 1
-                elif log['in'][-4] == 1:
-                    aichoice = 2
-                elif log['in'][-4] == 2:
-                    aichoice = 0
-            elif randint(0,8) == 8:
-                if log['in'][-1] == 0:
-                    aichoice = 1
-                elif log['in'][-1] == 1:
-                    aichoice = 2
-                elif log['in'][-1] == 2:
-                    aichoice = 0
-            elif aichoicec >= 4 and aichoicec <= 5:
-                a = log['in'][-1]
-                j = 0
-                h = 0
-                ilist = []
-                while j < len(log['in'])-1:
-                    if log['in'][j] == a:
-                        ilist.append([log['in'][j],log['in'][j+1]])
-                    j += 1
-                try:
-                    aichoice = max(ilist)[1]
-                except:
-                    aichoice = ilist[0]
-
-            elif aichoicec == 3:
-                if log['in'][-1] == 0:
-                    aichoice = 1
-                elif log['in'][-1] == 1:
-                    aichoice = 2
-                elif log['in'][-1] == 2:
-                    aichoice = 0
-            elif log['in'][-1] == log['in'][-3] and log['in'][-2] == log['in'][-4]:
-                if log['in'][-2] == 0:
-                    aichoice = 1
-                elif log['in'][-2] == 1:
-                    aichoice = 2
-                elif log['in'][-2] == 2:
-                    aichoice = 0
-            elif log['in'][-1] == log['in'][-4] and log['in'][-2] == log['in'][-5]:
-                if log['in'][-3] == 0:
-                    aichoice = 1
-                elif log['in'][-3] == 1:
-                    aichoice = 2
-                elif log['in'][-3] == 2:
-                    aichoice = 0
-            elif log['in'][-1] == log['in'][-3] and randint(0,6) == 1:
-                if log['in'][-2] == 0:
-                    aichoice = 1
-                elif log['in'][-2] == 1:
-                    aichoice = 2
-                elif log['in'][-2] == 2:
-                    aichoice = 0
-
-            elif aichoicec >= 0 and aichoicec <= 2:
-                i = randint(0,3)
-                if i == 3:
-                    if log['result'][-1] == 0:
-                        if log['in'][-1] == 0:
-                            aichoice = 2
-                        elif log['in'][-1] == 1:
-                            aichoice = 0
-                        elif log['in'][-1] == 2:
-                            aichoice = 1
-                    elif log['result'][-1] == 1:
-                        if log['in'][-1] == 0:
-                            aichoice = 1
-                        elif log['in'][-1] == 1:
-                            aichoice = 2
-                        elif log['in'][-1] == 2:
-                            aichoice = 0
-                    elif log['result'][-1] == -1:
-                        if log['in'][-1] == 0:
-                            aichoice = 0
-                        elif log['in'][-1] == 1:
-                            aichoice = 1
-                        elif log['in'][-1] == 2:
-                            aichoice = 2
-                elif i == 2:
-                    if log['in'][-1] == log['in'][-2]:
-                        if log['in'][-1] == 0:
-                            aichoice = 1
-                        elif log['in'][-1] == 1:
-                            aichoice = 2
-                        elif log['in'][-1] == 2:
-                            aichoice = 0
-                else:
-                    aichoice = max(log['in'])
-    else:
-        i = randint(0,1)
-        if i == 1:
-            if log['result'][-1] == 0:
-                if log['in'][-1] == 0:
-                    aichoice = 2
-                elif log['in'][-1] == 1:
-                    aichoice = 0
-                elif log['in'][-1] == 2:
-                    aichoice = 1
-            elif log['result'][-1] == 1:
-                if log['in'][-1] == 0:
-                    aichoice = 1
-                elif log['in'][-1] == 1:
-                    aichoice = 2
-                elif log['in'][-1] == 2:
-                    aichoice = 0
-            elif log['result'][-1] == -1:
-                if log['in'][-1] == 0:
-                    aichoice = 0
-                elif log['in'][-1] == 1:
-                    aichoice = 1
-                elif log['in'][-1] == 2:
-                    aichoice = 2
-        aichoice = randint(0,2)
-
-    result = main([choice, aichoice])
-    log['ch'].append(aichoice)
-    log['result'].append(result)
-    log['in'].append(choice)
-    if result == -1:
-        aiwin += 1
-    if result == 1:
-        playerwin += 1
-    print(str(playerwin)+ " - " + str(aiwin))
-    print('Player Choice : {}\nAI Choice : {}'.format(p[choice], p[aichoice]))
-    sleep(1)
-    system('clear')
-    print(str(playerwin)+ " - " + str(aiwin))
-    print('Player Choice : {}\nAI Choice : {}'.format(p[choice], p[aichoice]))
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
